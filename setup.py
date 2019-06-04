59a326504a9709d6b7ff17ed4eeeb73a358383e9
RyanSquared <ryan@hashbang.sh>
2019-06-04 16:58:51 -0500

    setup.py: Include subpackages

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 setup(
     name="mediapanel",
     version="0.1.0",
-    packages=["mediapanel"],
+    packages=["mediapanel", "mediapanel.config", "mediapanel.db"],
     install_requires=["sqlalchemy"]
 )
