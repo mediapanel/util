07e7a497699b6fe9d02afd2139d2e2faf7ffa0f1
RyanSquared <ryan@hashbang.sh>
2019-06-04 15:55:20 -0500

    mediapanel,setup.py: Rename package, add initial setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+# pylint: disable-all
+
+from distutils.core import setup
+
+setup(
+    name="mediapanel",
+    version="0.1.0",
+    packages=["mediapanel"],
+    install_requires=["sqlalchemy"]
+)
